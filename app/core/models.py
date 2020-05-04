ddd9c3ef04a8d5835399b81cf5bad144c343c425
NirupamDebnath <nirupamdebnath4@gmail.com>
2020-05-05 01:10:03 +0530

    Added recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -60,3 +60,23 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    """Recipe object"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    ingredients = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField("Tag")
+
+    # if you use models.ManyToManyField(Tag) with out string then
+    # classes has to be defined in proper order
+
+    def __str__(self):
+        return self.title
