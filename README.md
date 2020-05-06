a0eea3e7a2bc9aaa15156611cfe400c9bf4d307f
NirupamDebnath <nirupamdebnath4@gmail.com>
2020-05-07 01:55:28 +0530

    Added image field to recipe model

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ Recipe app api source code
 1. docker images
 2. docker-compose run --rm app sh -c "python manage.py startapp recipe"
 3. docker-compose up
-4. docker-compose run app sh -c "python manage.py makemigrations"
-5. docker-compose run app sh -c "python manage.py test && flake8"
+4. docker-compose run --rm app sh -c "python manage.py makemigrations"
+5. docker-compose run --rm app sh -c "python manage.py test && flake8"
 6. git commit -am "To commit in one line"
--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,17 @@
+import uuid
+import os
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
     PermissionsMixin
 from django.conf import settings
+
+
+def recipe_image_file_path(instance, filename):
+    """Generated file path for new recipe image"""
+    ext = filename.split(".")[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+
+    return os.path.join('uploads/recipe/', filename)
 
 
 class UserManager(BaseUserManager):
@@ -74,7 +84,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     ingredients = models.ManyToManyField('Ingredient')
     tags = models.ManyToManyField("Tag")
-
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
     # if you use models.ManyToManyField(Tag) with out string then
     # classes has to be defined in proper order
 
